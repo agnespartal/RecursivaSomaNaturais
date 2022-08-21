ed5456d97c9735aa5802171425fafe47db44cfc4
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-08-21 18:59:41 -0300

    Update SomaController.java

--- Exec1/src/Controller/SomaController.java
+++ Exec1/src/Controller/SomaController.java
@@ -7,11 +7,11 @@ public class SomaController {
 	}
 
 	public int somaNaturais (int num) {
-		
+		//Condição de parada quando valor inicial chegar a zero
 		if(num <= 0) {
 			return 0;
 		} else
-			
 			return num + somaNaturais(num -1) ;
+		//soma o valor inicial diminuindo em um a cada chamada
 	}
 }
